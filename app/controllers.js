91952d6eb92c1e5e744da708a97d0088f17a990b
Peter Nolan <peternolan@Peters-MacBook-Pro.local>
2019-07-12 16:20:19 -0400

    Implemented Collapsing

--- app/controllers.js
+++ app/controllers.js
@@ -18,6 +18,17 @@ angular.module('navApp.controllers', []).controller('navController', function ($
        "ExpenseAuthorization"
    ];
 
+    $scope.toggleSearch = function(){
+        $("#search").toggleClass("search-input-open").focus();
+    };
+
+    $scope.openTab = function(page) {
+        console.log("HOVER");
+        console.log(page);
+        $("#"+page).toggleClass("nav-id-open");
+    };
+
+
    $scope.nav = function (page, src) {
 
        $scope.currentNavItem = barService.goTo(page);
