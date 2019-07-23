29b1dc5276bbe237a857a4c4c4c3b1457312e1f0
Peter Nolan <peternolan@Peters-MacBook-Pro.local>
2019-07-23 09:51:00 -0400

    Changed header and button

--- app/page/page.js
+++ app/page/page.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // Declare app level module which depends on views, and core components
-angular.module('navApp', []).component('page', {
+angular.module('navApp').component('page', {
 
-    templateUrl: '../page/page.html',
+    templateUrl: 'page/page.html',
     bindings: {
         currentNavItem: '='
     }
